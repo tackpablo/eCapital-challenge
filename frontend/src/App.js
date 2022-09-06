60fc330879d2ff550b13096e993af2c33b62690f
Pablo <tackpablo@gmail.com>
2022-09-06 00:12:41 -0700

    add initialize employee list on load, components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,40 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./components/HomePage";
+import EmployeeList from "./components/EmployeeList";
 
 function App() {
+    const [employees, setEmployees] = useState();
+
+    // Initialize employee list on load
+    useEffect(() => {
+        employeeList();
+    }, []);
+
+    // Fetch employee list
+    async function employeeList() {
+        const url = "/api/employees/";
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+
+            console.log("DATA: ", data);
+            setEmployees(data.returnedData);
+            console.log("EMPLOYEES DATA RETURNED");
+        } catch (err) {
+            console.log("ERROR FETCHING EMPLOYEES DATA", err);
+        }
+    }
+
     return (
         <div className="App">
             <Router>
                 <div>
                     <Routes>
-                        <Route path="/" element={<HomePage />} />
+                        <Route
+                            path="/"
+                            element={<EmployeeList employees={employees} />}
+                        />
                     </Routes>
                 </div>
             </Router>
--- frontend/src/components/EmployeeListItem.jsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-const EmployeeListItem = () => {
-    return <div>Employee</div>;
-};
-
-export default EmployeeListItem;
