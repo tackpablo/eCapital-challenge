e446e8587f4d0b7e96cd34356fcd6c864c0949df
Pablo <tackpablo@gmail.com>
2022-09-05 20:49:35 -0700

    add employees table schema and seeds

---
+++ backend/db/seeds/01_employees.sql
@@ -0,0 +1,5 @@
+-- Employees table seeds here (Example)
+INSERT INTO Employees (first_name, last_name, salary) VALUES ('Lewis', 'Burson', 40700), ('Ian', 'Malcolm', 70000), ('Ellie', 'Sattler', 102000), ('Dennis', 'Nedry', 52000), ('John', 'Hammond', 89600), ('Ray', 'Arnold', 45000), ('Laura', 'Burnett', 80000)
+;
+
+
