cfe35b6bd6c671515eeaf5de90638e58296d2550
Pablo <tackpablo@gmail.com>
2022-09-05 21:46:56 -0700

    add database queries for all BREAD operations, test with postman

--- backend/routes/employees.js
+++ backend/routes/employees.js
@@ -4,27 +4,74 @@ const router = express.Router();
 module.exports = (db) => {
     // GET: BROWSE --- RETRIEVE ALL EMPLOYEE
     router.get("/", (req, res) => {
-        res.send("ALL EMPLOYEES");
+        const queryParams = [];
+        const queryStr = `SELECT * FROM employees ORDER BY id ASC`;
+
+        db.query(queryStr, queryParams, (error, results) => {
+            if (error) {
+                throw error;
+            }
+            res.status(200).json(results.rows);
+        });
     });
 
     // GET: READ --- RETRIEVE SPECIFIC EMPLOYEE BY ID
     router.get("/:id", (req, res) => {
-        res.send("EMPLOYEE BY ID");
+        const { id } = req.params;
+        const queryParams = [id];
+        const queryStr = `SELECT * FROM employees WHERE id = $1`;
+
+        db.query(queryStr, queryParams, (error, results) => {
+            if (error) {
+                throw error;
+            }
+            res.status(200).json(results.rows);
+        });
     });
 
     // PUT/PATCH: EDIT --- UPDATE DATA FOR SPECIFIC EMPLOYEE ID
     router.put("/:id", (req, res) => {
-        res.send("EDIT EMPLOYEE");
+        const { id } = req.params;
+        const { first_name, last_name, salary } = req.body;
+        const queryParams = [first_name, last_name, salary, id];
+        const queryStr = `UPDATE employees SET first_name = $1, last_name = $2, salary = $3 WHERE id = $4`;
+
+        db.query(queryStr, queryParams, (error, results) => {
+            if (error) {
+                throw error;
+            }
+            res.status(200).send(`Employee modified with ID: ${id}`);
+        });
     });
 
     // POST: ADD --- ADD NEW EMPLOYEE
-    router.post("/:id", (req, res) => {
-        res.send("ADD NEW EMPLOYEE");
+    router.post("/", (req, res) => {
+        const { first_name, last_name, salary } = req.body;
+        const queryParams = [first_name, last_name, salary];
+        const queryStr = `INSERT INTO employees (first_name, last_name, salary) VALUES ($1, $2, $3) RETURNING *;`;
+
+        db.query(queryStr, queryParams, (error, results) => {
+            if (error) {
+                throw error;
+            }
+            res.status(201).send(
+                `Employee added with ID: ${results.rows[0].id}`
+            );
+        });
     });
 
     // DELETE: DELETE --- DELETE EMPLOYEE
     router.delete("/:id", (req, res) => {
-        res.send("DELETE EMPLOYEE");
+        const { id } = req.params;
+        const queryParams = [id];
+        const queryStr = `DELETE FROM employees WHERE id = $1 RETURNING *;`;
+
+        db.query(queryStr, queryParams, (error, results) => {
+            if (error) {
+                throw error;
+            }
+            res.status(200).send(`Employee deleted with ID: ${id}`);
+        });
     });
 
     return router;
